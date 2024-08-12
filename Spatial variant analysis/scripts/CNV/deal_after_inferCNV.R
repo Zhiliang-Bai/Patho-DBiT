15257514b4b60ab2570f251a4cc586da09831066
Dingyao Zhang <59897897+dingyaozhang@users.noreply.github.com>
2024-08-12 17:04:19 -0400

    Update deal_after_inferCNV.R

--- Spatial variant analysis/scripts/CNV/deal_after_inferCNV.R
+++ Spatial variant analysis/scripts/CNV/deal_after_inferCNV.R
@@ -1,7 +1,18 @@
 library(infercnv)
 
 matin = "result/CNV/inferCNV/result/infercnv.20_HMM_predHMMi6.leiden.hmm_mode-subclusters.Pnorm_0.5.repr_intensities.observations.txt"
-cluster_file = "scRNAmir/data/FFPE/Clusters/Cluster_and_spatial.id_LM0623_Large.csv"
+# An example of Cluster_and_spatial.id.csv
+#"","SCT_snn_res.1.2"
+#"100x1","8"
+#"100x2","4"
+#"100x3","15"
+#"100x4","15"
+#"100x5","4"
+#"100x6","3"
+#"100x7","18"
+#"100x8","8"
+#"100x9","3"
+cluster_file = "scRNAmir/data/FFPE/Clusters/Cluster_and_spatial.id.csv"
 
 
 datain = read.table(matin, header = TRUE, sep = ' ', stringsAsFactors = FALSE)
